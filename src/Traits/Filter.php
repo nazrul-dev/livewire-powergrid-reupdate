fb597b24c329fa669f7b791b2a178bf9af6292a9
nazrul <76646247+nazrul-dev@users.noreply.github.com>
2023-07-23 11:04:14 +0800

    Update Filter.php

--- src/Traits/Filter.php
+++ src/Traits/Filter.php
@@ -115,19 +115,14 @@ trait Filter
 
     public static function getInputTextOptions(): array
     {
-        return [
+            return [
             'contains',
             'contains_not',
             'is',
             'is_not',
             'starts_with',
             'ends_with',
-            'is_empty',
-            'is_not_empty',
-            'is_null',
-            'is_not_null',
-            'is_blank',
-            'is_not_blank',
+ 
         ];
     }
 
